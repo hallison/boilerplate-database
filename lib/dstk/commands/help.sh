60f41e2f678117ce2a4aa6cf9df95d360f847bb9
Hallison Batista <hallisonbatista@gmail.com>
2023-02-22 20:50:30 -0400

    Environment configuration

--- lib/dstk/commands/help.sh
+++ lib/dstk/commands/help.sh
@@ -10,7 +10,7 @@ function dstk-help {
   #? Show usage message
   dstk_doc_header $0
 
-  for command_file in $LIBDIR/commands/*.sh; do
+  for command_file in ${DSTK_COMMANDS_LIBDIR}/*.sh; do
     dstk_doc_markup $command_file
   done
 }
---
+++ lib/dstk/environment.sh
@@ -0,0 +1,5 @@
+export DSTK_RC=.dstkrc
+export DSTK_PATH=${DSTK_PATH:-.}
+export DSTK_DATASOURCES_PATH=${DSTK_PATH}/datasources
+export DSTK_SCHEMA_PATH=${DSTK_PATH}/schema
+export DSTK_DATASOURCE_ENABLED=${DSTK_DATASOURCES_PATH}/enabled.input
