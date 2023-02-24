003e0c977a5c8b29dd659da4d41181b5194cef0f
Hallison Batista <hallisonbatista@gmail.com>
2023-02-23 21:49:18 -0400

    Initialize command checks the configuration
    - The command to initialize will be canceled when the configuration file
      is found.

--- lib/dstk/commands/init.sh
+++ lib/dstk/commands/init.sh
@@ -1,12 +1,17 @@
-function dstk-init { #: [DATA_PATH]
+function dstk-init { #: [PATH]
   #? Initialize Datasource Toolkit configurations.
   #? This command creates the ".dstkrc" file that will contain the variables
   #? that will be used by the "dstk" command.
-  : # ${1:?Datasource name}
+  : ${1:-datasource}
+
+  dstk_check_configuration && {
+    echo "Datasource Toolkit has been initialized in directory '${DSTK_PATH}'."
+    return 1
+  }
 
   test -n "${1}" && {
     export DSTK_PATH="${1}"
-    source ${DSTK_ENVIRONMENT}
+    source "${DSTK_ENVIRONMENT}"
     echo "DSTK_PATH=${DSTK_PATH}" > "${DSTK_RC}"
   }
 
@@ -16,10 +21,8 @@ function dstk-init { #: [DATA_PATH]
   printf "Directory ${DSTK_SCHEMA_PATH} ... "
   mkdir -p "${DSTK_SCHEMA_PATH}" && echo done
 
-  echo
-  echo "Datasource Toolkit has been initialized in directory ${1:-${PWD}}."
-  echo "To configure a datasource, run command:"
-  echo "$ dstk config <DATASOURCE>"
+  echo "Datasource Toolkit has been initialized in directory '${DSTK_PATH}'."
+  echo "To configure a datasource, run command 'config'."
 
   return 0
 }
